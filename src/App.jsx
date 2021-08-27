4d8584f39ec0976b030285a4a4f9717dde527a2e
Lukas <lukas.ancikevicius@gmail.com>
2021-08-27 09:25:56 +0300

    newProject

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,9 @@
-import { Route, Switch, Redirect } from 'react-router-dom';
-
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import MainHeader from './components/MainHeader';
-import ProductDetails from './pages/ProductDetails';
-
 function App() {
   return (
     <div>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/welcome" />
-          </Route>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route path="/products" exact>
-            <Products />
-          </Route>
-          <Route path="/products/:product">
-            <ProductDetails />
-          </Route>
-        </Switch>
-      </main>
+
     </div>
   );
 }
 
 export default App;
-
-// our-domain.com/welcome => Welcome Component
-// our-domain.com/products => Products Component
